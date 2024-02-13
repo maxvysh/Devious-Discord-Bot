cebad0988b792e85641f8fa9f132eff624997bf2
maxvysh <45989000+maxvysh@users.noreply.github.com>
2024-02-13 00:35:51 -0500

    Making this repo public

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Devious Discord Bot
  
+This was a discord bot I orignally made just to mess with my friends, but I decided to publish it because honestly it had some impressive features and it could be a good framework for anyone who is trying to build something similar. 
+Make sure to put in all your own unique values in the .env file for the bot to work properly. 
+
+FEATURES:
+ValMsg.js sends an image whenever someone types Val or Valorant in a chat channel
+ready.js finds the general chat channel of the server you put in .env and then the bot will send anything you type into the terminal
+lolstas.js displays a player's profile icon and kda for League of Legends
+defcon_4.js find anyone playing valorant in the discord server and then @'s them to stop playing that awful game
+defcon_1.js will ask anyone but the .env declared owner for a passcode, whatever they put next won't matter, the bot will tell them 'wrong passcode.' But if the owner activates defcon1, then the bot joins the voice channel they are in and starts blaring an air raid alarm and counting down in the text channel. Following the 10 second countdown, all the voice channels (NOT TEXT CHANNELS, THOSE STAY) will be deleted. 
+muteme.js was just a test command but I left it anyway. The command is self explanitory. 
--- events/valMsg.js
+++ events/valMsg.js
@@ -15,18 +15,19 @@ module.exports = {
         
                 previousRandomNumber = randomNumber;
         
+                // Insert your own images here, whenver someone says Val or Valorant, the bot will send a random image from the ones you input
                 switch (randomNumber) {
                     case 1:
-                        message.reply('https://cdn.discordapp.com/attachments/508827545890717696/1188376531702136868/artwork.png?ex=659a4ce4&is=6587d7e4&hm=70fd084f319c5222dccc79f2351fdd82523e26b00fae05f2057572b41bbf0ecc&');
+                        message.reply('');
                         break;
                     case 2:
-                        message.reply('https://cdn.discordapp.com/attachments/808932976125149225/1188376920451186718/images.png?ex=659a4d40&is=6587d840&hm=a1bd7d6dd8ecf850527b681bc09221a4d0ab8850172e04b050808d3f1abcdc3a&');
+                        message.reply('');
                         break;
                     case 3:
-                        message.reply('https://cdn.discordapp.com/attachments/808932976125149225/1188376973135851550/sweet_relief___3_by_slimepope_dfyk73e-350t.png?ex=659a4d4d&is=6587d84d&hm=759db659688f00da649ca29718fb935ff960a1c443e454fd4f06a780b6c334a3&');
+                        message.reply('');
                         break;
                     case 4:
-                        message.reply('https://cdn.discordapp.com/attachments/808932976125149225/1188377052957642784/ajcfj91wmalb1.png?ex=659a4d60&is=6587d860&hm=2619da327a3cd6b4901f75c0647dc9f88ad04109041fbc851ab420b6da4ea11a&');
+                        message.reply('');
                         break;
                 }
             }
